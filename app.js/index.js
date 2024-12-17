0311878b43f0e057ca81ff2a5fbfbb13bdc79e31
sirarun <annipal141297@gmail.com>
2024-12-17 21:53:27 +0530

    first project

---
+++ app.js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll",function(){
+  if(window.pageYOffset>4000){
+      document.getElementById("one").style.position = "fixed"
+      document.getElementById("one").style.top = "0"
+
+  }
+  
+
+})
